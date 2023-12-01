978098f7b04afb87a3be76812682d950e1c0bf3a
ProsESportu <t.hajduga@poczta.fm>
2023-12-01 18:40:38 +0100

    gkjgkj

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Loader } from '@googlemaps/js-api-loader';
+	import { PUBLIC_GMA } from '$env/static/public';
+	let map;
+	let mel: HTMLElement;
+	onMount(async () => {
+		const loader = new Loader({
+			apiKey: PUBLIC_GMA,
+			version: 'weekly'
+		});
+
+		loader.load().then(async () => {
+			const { Map } = (await google.maps.importLibrary('maps')) as google.maps.MapsLibrary;
+			map = new Map(mel, {
+				center: { lat: -34.397, lng: 150.644 },
+				zoom: 8
+			});
+		});
+	});
+</script>
+
+<div bind:this={mel} class="h-screen"></div>
