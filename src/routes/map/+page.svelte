e54e8dfb5219f03dac3b7de62d53fea33bed2695
ProsESportu <t.hajduga@poczta.fm>
2023-12-01 23:02:45 +0100

    gss

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { PUBLIC_GMA } from '$env/static/public';
+	import { db } from '$lib/db';
 	import { Loader } from '@googlemaps/js-api-loader';
+	import { GeoPoint, collection, getDocs, query } from 'firebase/firestore';
 	import { onMount } from 'svelte';
-	let map:google.maps.Map;
+	let map: google.maps.Map;
 	let mel: HTMLElement;
 	onMount(async () => {
 		const loader = new Loader({
@@ -12,11 +14,28 @@
 
 		loader.load().then(async () => {
 			const { Map } = (await google.maps.importLibrary('maps')) as google.maps.MapsLibrary;
+			const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+				'marker'
+			)) as google.maps.MarkerLibrary;
+
 			map = new Map(mel, {
 				center: { lat: 49.60964693872272, lng: 20.7039616596972 },
-				zoom: 16
+				zoom: 16,
+				mapId:"map_id"
 			});
-			
+			const coll = collection(db, '1');
+			const docs = await getDocs(query(coll));
+			docs.forEach((e) => {
+				const cord: GeoPoint = e.data()['cords'];
+				const txt=document.createElement("p")
+				txt.innerHTML="test"
+				const marker = new AdvancedMarkerElement({
+					map,
+					position: { lat: cord.latitude, lng: cord.longitude },
+					title:"utopienie",
+					//content:txt
+				});
+			});
 		});
 	});
 </script>
