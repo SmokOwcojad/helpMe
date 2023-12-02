6465d59527bf5a2f96ce2e30dbe96408d09e6ee9
SmokOwcojad <exstra.jan@gmail.com>
2023-12-02 01:54:18 +0100

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,20 +13,20 @@
 			slotEnd="place-content-end"
 			slotCenter="place-content-center"
 		>
-			<svelte:fragment slot="lead"
-				><img src="icon-rectangle-NoBg-rect.png" alt="icon" width="40%" /></svelte:fragment
-			>
+			<svelte:fragment slot="lead">
+				<a href="/"><img src="icon-rectangle-NoBg-rect.png" alt="icon" width="40%"/></a>
+			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<LightSwitch slot="end" class=" mx-5" />
-				<button type="button" class="btn variant-filled rounded-full">
+				<a href="/map"><button type="button" class="btn variant-filled rounded-full">
 					<img src="map.png" alt="mapa" class="w-20" />
 					Przejdź do mapy
 				</button>
+			</a>
 				<img src="man.png" alt="man" width="10%" slot="trail" />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 
-	<slot/>
+	<slot />
 </AppShell>
-
