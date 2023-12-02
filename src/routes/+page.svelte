85476cdfffb5209a02dd5b846bcb38d1825ccbbf
ProsESportu <t.hajduga@poczta.fm>
2023-12-02 01:59:42 +0100

    uhety

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,13 +13,13 @@
       }
     });
   
-    function getCookie(name) {
+    function getCookie(name:string) {
       const value = `; ${document.cookie}`;
       const parts = value.split(`; ${name}=`);
       if (parts.length === 2) return parts.pop().split(';').shift();
     }
   
-    function setCookie(name, value, days) {
+    function setCookie(name:string, value:string, days:number) {
       const expires = new Date();
       expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
       document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
@@ -30,7 +30,7 @@
       helpCount++;
   
       // Ustaw nową wartość ciasteczka
-      setCookie('helpCount', helpCount, 365);
+      setCookie('helpCount', helpCount.toString(), 365);
   
       // Zaktualizuj widok
       const counterElement = document.getElementById('helpCounter');
